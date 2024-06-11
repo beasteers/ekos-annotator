6f5c0dc6e8fa825e420e3c0379ce6e273b327832
Iran R. Roman <iran@ccrma.stanford.edu>
2024-06-11 09:35:24 -0400

    Add files via upload

--- scripts/prepare_batches.py
+++ scripts/prepare_batches.py
@@ -1,3 +1,52 @@
+import pandas as pd
+
+def load_and_shuffle_df(path):
+    df = pd.read_csv(path)
+    df = df.groupby('noun').apply(lambda x: x.sample(frac=1)).reset_index(drop=True)
+    return df
+
+def create_batches_df(N,df):
+
+    # Prepare new DataFrame
+    column_names = ['noun']
+    for i in range(1, N+1):
+        column_names.append(f'image_{i}_file_name')
+        column_names.append(f'image_{i}_polygons')
+    new_df = pd.DataFrame(columns=column_names)
+
+    df_noun = df.groupby('noun')
+
+    for name, group in df_noun:
+        total_rows = len(group)
+
+        # Populate the new DataFrame
+        for start_idx in range(0, total_rows-N, N):
+            row_data = {'noun':name}
+            for i in range(N):
+                if start_idx + i < total_rows:
+                    current_row = group.iloc[start_idx + i]
+                    row_data[f'image_{i+1}_file_name'] = f"{current_row['narration_id']}_{current_row['frame_id']}.jpg"
+                    polys = eval(current_row['polygons'])[0].decode()
+                    polys = polys.replace('][', ']@[').split('@')
+                    polys = [eval(poly) for poly in polys]
+                    polys = [[c[0] for c in p] for poly in polys for p in poly]
+                    row_data[f'image_{i+1}_polygons'] = polys
+            new_df = pd.concat([new_df, pd.DataFrame([row_data])], ignore_index=True)
+    return new_df
+
+def main(path_to_mturk_csv='mturk_noun_data.csv', batch_size = 9):
+
+    df = load_and_shuffle_df(path_to_mturk_csv)
+
+    batches_df = create_batches_df(batch_size,df)
+
+    batches_df.to_csv('mturk_batches.csv')
+
+
+if __name__ == "__main__":
+    import fire
+    fire.Fire(main)
+
 '''
 
 Creates a CSV with these columns:
@@ -25,5 +74,4 @@ image_6_polygons = '[[...],[...]]'
 image_7_polygons = '[[...],[...]]'
 image_8_polygons = '[[...],[...]]'
 image_9_polygons = '[[...],[...]]'
-
 '''
